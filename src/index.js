f1bc4fb2f76d3425747768c6d80a3d78cdb00e42
wp-jiang <wpjiangb135@yandex.com>
2025-03-13 17:26:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -171,3 +171,16 @@ function responseUnauthorized(url) {
     headers: headers,
   });
 }
+
+
+import DOCS from './help.html'
+ 
+// return docs
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
